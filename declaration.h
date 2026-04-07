c38b631b87c43e8552119e108db5d63bec804f71
SitrakaTokyHariliva <sitrakarzf@gmail.com>
2026-04-07 05:53:47 +0200

    Create declaration.h

---
+++ declaration.h
@@ -0,0 +1,13 @@
+#ifndef RESOLUTION_H
+#define RESOLUTION_H
+
+#define EPS 0.0000001
+
+double f(double x);
+double dichotomie(double a, double b);
+double newton(double x0);
+
+double aire_rectangles(double a, double b, int n);
+double aire_trapezes(double a, double b, int n);
+
+#endif

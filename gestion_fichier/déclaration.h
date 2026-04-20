1db77a5102c49414cb18aa264302f25bfb717787
SitrakaTokyHariliva <sitrakarzf@gmail.com>
2026-04-20 14:41:22 +0200

    Update déclaration.h

--- gestion_fichier/déclaration.h
+++ gestion_fichier/déclaration.h
@@ -10,10 +10,10 @@
 
 void lecture_fichier(char tab[MAX_ETUDIANTS][MAX_CHAMPS][MAX_CAR]);
 
-
-void enregistrer_fichier(char tab[MAX_ETUDIANTS][MAX_CHAMPS][MAX_CAR], int nb_etudiants);
-
+void info_etudiants(tab[MAX_ETUDIANTS][4][50], char recherche[MAX_CAR];
 
 void supprimer_etudiant(char tab[MAX_ETUDIANTS][MAX_CHAMPS][MAX_CAR], int *nb_actuel, char recherche[MAX_CAR]);
+
+void enregistrer_fichier(char tab[MAX_ETUDIANTS][MAX_CHAMPS][MAX_CAR], int nb_etudiants);
 
 #endif
